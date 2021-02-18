ff97ec4271c2e186903257524ca084f84df3310f
Alexandr Menshikov <menal2016@mail.ru>
2021-02-18 21:00:05 +0700

    Format .clang-format chessviz.c

--- chessviz.c
+++ chessviz.c
@@ -19,7 +19,7 @@ typedef struct {
     Piece board[8][8];
 } Board;
 
-void Visual_Board(Board *cl) 
+void Visual_Board(Board* cl)
 {
     cl->board[0][0] = W_Rook;
     cl->board[0][1] = W_Knight;
@@ -31,14 +31,14 @@ void Visual_Board(Board *cl)
     cl->board[0][7] = W_Rook;
     for (int i = 0; i < 8; ++i) {
         cl->board[1][i] = W_Pawn;
-  }
+    }
     for (int i = 2; i < 6; i++) {
         for (int j = 0; j < 8; ++j)
             cl->board[i][j] = None;
-  }
+    }
     for (int i = 0; i < 8; ++i) {
         cl->board[6][i] = B_Pawn;
-  }
+    }
 
     cl->board[7][0] = B_Rook;
     cl->board[7][1] = B_Knight;
@@ -49,25 +49,23 @@ void Visual_Board(Board *cl)
     cl->board[7][6] = B_Knight;
     cl->board[7][7] = B_Rook;
 }
-void Print_Board(Board *cl) 
-{ 
+void Print_Board(Board* cl)
+{
     int count = 9;
     for (int i = 0; i < 8; ++i, putchar('\n')) {
         printf("%d", count - 1);
-            for (int j = 0; j < 8; ++j) {
-                printf("%c", cl->board[i][j]);
+        for (int j = 0; j < 8; ++j) {
+            printf("%c", cl->board[i][j]);
+        }
+        count--;
     }
-            count--;
-  }
     printf(" abcdefgh\t");
 }
 
-
-int main() 
+int main()
 {
     Board Board;
     Visual_Board(&Board);
     Print_Board(&Board);
     return 0;
 }
-
