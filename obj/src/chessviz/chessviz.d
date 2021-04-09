c18870d8a89f104c0dfc9dd0935c08808c09f1ee
Alexandr Menshikov <menal2016@mail.ru>
2021-04-09 11:28:09 +0700

    Delete files .d and .a

--- obj/src/chessviz/chessviz.d
+++
@@ -1,4 +0,0 @@
-obj/src/chessviz/chessviz.o: src/chessviz/chessviz.c \
- src/libchessviz/board.h src/libchessviz/board_type.h \
- src/libchessviz/move.h src/libchessviz/Move_pawn.h \
- src/libchessviz/board.h src/libchessviz/verification.h
