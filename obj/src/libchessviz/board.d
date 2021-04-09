bfa09d6722bf901287a3d0276252a0459cd469c0
Alexandr Menshikov <menal2016@mail.ru>
2021-04-09 11:29:06 +0700

    Delete files.d and .a

--- obj/src/libchessviz/board.d
+++
@@ -1,2 +0,0 @@
-obj/src/libchessviz/board.o: src/libchessviz/board.c \
- src/libchessviz/board.h src/libchessviz/board_type.h
--- obj/src/libchessviz/verification.d
+++
@@ -1,3 +0,0 @@
-obj/src/libchessviz/verification.o: src/libchessviz/verification.c \
- src/libchessviz/verification.h src/libchessviz/board_type.h \
- src/libchessviz/board.h
