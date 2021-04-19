4ec7b80b4d8b8ebfe53c174b32702263561d16c1
Alexandr Menshikov <menal2016@mail.ru>
2021-04-19 22:40:49 +0700

    Add func King  mowe and walk fot file.h

---
+++ src/libchessviz/Move_King.h
@@ -0,0 +1,11 @@
+
+#ifndef MOVE_KING_H
+#define MOVE_KING_H
+
+#include "board.h"
+#include "verification.h"
+
+void King_Walk(Piece num, Board* cl, char* str);
+void Move_King(Board* cl, char* str, int num);
+
+#endif
