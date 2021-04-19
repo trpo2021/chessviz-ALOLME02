f3ba40e4a589a11ba399149c76c8d1c321732db2
Alexandr Menshikov <menal2016@mail.ru>
2021-04-19 22:37:12 +0700

    Add func Bishop  mowe and walk fot file.h

---
+++ src/libchessviz/Move_Bishop.h
@@ -0,0 +1,10 @@
+#ifndef MOVE_BISHOP_H
+#define MOVE_BISHOP_H
+
+#include "board.h"
+#include "verification.h"
+
+void Bishop_Walk(Piece num, Board* cl, char* str);
+void Move_Bishop(Board* cl, char* str, int num);
+
+#endif
