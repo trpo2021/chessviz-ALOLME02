ee2668dd780028151fee10a57251ffd6418ba87d
Alexandr Menshikov <menal2016@mail.ru>
2021-05-04 14:02:50 +0700

    Setting file.h by Create function which Return field cordinates x1, y1, x2, y2, to remove many duplicate conditions

---
+++ src/libchessviz/Setting_Field.h
@@ -0,0 +1,10 @@
+#ifndef SETTING_FIELD_H
+#define SETTING_FIELD_H
+
+#include "board.h"
+
+int Seting_Field_x(char* str);
+int Seting_Field_y(char* str);
+int Seting_Field_x2(char* str);
+int Seting_Field_y2(char* str);
+#endif
